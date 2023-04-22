55b0ed774c3e0d47c3381611a2a29224c4faf6a4
Heitor Figueiredo <heitorfigbr@gmail.com>
2023-04-22 17:03:58 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const getVariation = () => {
 }
 
 const App = () => {
-  const cashkonto = 1890;
+  const cashkonto = 1750;
   const formattedCashkonto = cashkonto.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' });
 
   const depot = 1100;
