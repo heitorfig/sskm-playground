ee4b4e5dcdcab2702b0597b3fd785b6fe93b21ff
Heitor Figueiredo <heitorfigbr@gmail.com>
2023-04-24 01:19:11 +0200

    Fixing colors

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const getVariation = () => {
 }
 
 const App = () => {
-  const cashkonto = 1700;
+  const cashkonto = 1685;
   const formattedCashkonto = cashkonto.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' });
 
   const depot = 1100;
