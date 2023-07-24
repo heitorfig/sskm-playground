9cbfa9f3ea9dc0d54721d8f9df2019dd940834eb
Heitor Figueiredo <hfigueiredo@simscale.com>
2023-07-24 10:22:55 +0200

    Updating trackjs configs

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const getVariation = () => {
 }
 
 const App = () => {
-  const cashkonto = 2175;
+  const cashkonto = 2125;
   const formattedCashkonto = cashkonto.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' });
 
   const depot = 1100;
